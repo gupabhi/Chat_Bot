3e5dbef9756966ee283027af785a6e6c6500e276
gupabhi <abhishekgup.1998@gmail.com>
2016-11-29 22:24:36 +0530

    Update fetching_P.py

--- fetching_P.py
+++ fetching_P.py
@@ -1,4 +1,4 @@
-import urllib, json
+import urllib, json, dictionary
 
 def fetch_P_url(prop_str):
 	a = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search="
@@ -11,9 +11,64 @@ def fetch_P_url(prop_str):
 	b = b + word_arr[l-1]
 	return (a+b+c)
 
-def send_P_id (P_val):
+def no_of_P_id (P_val):
+	#processing JSON doc
+	data = fetch_data(P_val)
+	return(len(data['search']))
+
+def fetch_data (P_val):
 	P_url = fetch_P_url(P_val)
 	response = urllib.urlopen(P_url)
 	data = json.loads(response.read())
-	#print data
-	return (data['search'][0]['id'])
+	return data
+
+def search (arr, str):
+	str = str.lower()
+	l = len(arr)
+	for i in range (0,l):
+		if (arr[i]['label'].lower() == str):
+			return i
+	return (-1)
+
+def use_dictionary (P_val):
+	syn_list = dictionary.fetch_syn (P_val)
+	for syn in syn_list:
+		if (no_of_P_id(syn) == 0):
+			syn_list.remove(syn)
+	l = len(syn_list)
+	if (l==0):
+		return ('P$#')
+	print('Did you mean?\n')
+	for syn in syn_list:
+		print (syn)
+	while (1):
+		str = raw_input('Enter option:\n')
+		k = search (syn_list, str)
+		if (k != -1):
+			break
+		print ('Invalid choice')
+	D = fetch_data(P_val)
+	P_id = work_P(no_of_P_id(str), D, str)
+	return P_id
+	
+
+def work_P (no_of_results, data, P_val): #I need the P_val in this function
+	if (no_of_results == 0):
+		return(use_dictionary(P_val)); #returns a list of synonyms found in the dictionary
+	if (no_of_results == 1):
+		return (data['search'][0]['id'])
+	print('Did you mean?\n')
+	for i in range (0, no_of_results):
+		print (data['search'][i]['label'])
+	while (1):
+		str = raw_input('Enter option:\n')
+		k = search (data['search'], str)
+		if (k != -1):
+			break
+		print ('Invalid choice')
+	return (data['search'][k]['id'])
+
+def final_f (P_val):
+	x = no_of_P_id(P_val)
+	D = fetch_data(P_val)
+	return (work_P(x,D,P_val))
